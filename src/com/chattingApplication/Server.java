d2567ad961cb1a82076fa521a42a139ae41d4da5
Anup81 <anupdhakal81@gmail.com>
2022-07-24 13:14:51 +0545

    Setting Up JPanel

--- src/com/chattingApplication/Server.java
+++ src/com/chattingApplication/Server.java
@@ -1,13 +1,27 @@
 package com.chattingApplication;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Server extends JFrame{
 
     Server(){
+
+        setLayout(null); //Don't use any layout from Swing and Make of your own so null
+
+        JPanel p1 = new JPanel(); // Panel is used what to do at upper frame
+        p1.setBackground(new Color(7, 94, 84));
+        p1.setBounds(0, 0, 450, 70); // For coordinates
+        add(p1);
+
+
         setSize(450, 700); // Providing Frame width and Height
-        setVisible(true); // Making frame visible
+
+        getContentPane().setBackground(Color.WHITE);
+
+
         setLocation(200, 50);
+        setVisible(true); // Making frame visible
 
     }
 
